de87119cf2591443216adc85ccc0e3d808a956a6
Mollydayne <cl.w.perez@gmail.com>
2025-10-26 16:32:20 +0100

    Repositionnement

--- client/src/components/MordechaiLogo.jsx
+++ client/src/components/MordechaiLogo.jsx
@@ -1,14 +1,13 @@
-// src/components/MordechaiLogo.jsx
 import React from "react";
 import mordechaiBlack from "../assets/mordechai_black.png";
 
 export default function MordechaiLogo() {
   return (
-    <div className="absolute top-[70px] left-6 z-10">
+    <div className="absolute top-[85px] left-6 z-10">
       <img
         src={mordechaiBlack}
         alt="Mordechai logo"
-        className="w-20 h-auto opacity-90 hover:opacity-100 transition-opacity duration-300"
+        className="w-24 h-auto opacity-90 hover:opacity-100 transition-opacity duration-300"
       />
     </div>
   );
